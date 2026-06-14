a99e17f97fd7d71c0de9ff2c0259b8bfefff3733
hqqqqg <2426291059@qq.com>
2026-06-14 11:41:14 +0800

    测试citylistparser

---
+++ crawler/zhenai/parser/citylist_test.go
@@ -0,0 +1,57 @@
+// 测试城市解析器
+package parser
+
+import (
+	"os"
+	"testing"
+)
+
+func TestParseCityList(t *testing.T) {
+	// contents, err := fetcher.Fetch(
+	// 	"http://www.zhenai.com/zhenghun")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// err = os.WriteFile("citylist_test_data.html", contents, 0644) //存文件
+
+	contents, err := os.ReadFile("citylist_test_data.html")
+	if err != nil {
+		panic(err)
+	}
+	result := ParseCityList(contents)
+	const resultSize = 22 //城市个数
+	//取三个
+	expectedUrls := []string{
+		"http://www.zhenai.com/zhenghun/aba",
+		"http://www.zhenai.com/zhenghun/baicheng1",
+		"http://www.zhenai.com/zhenghun/cangzhou",
+	}
+	expectedCities := []string{
+		"阿坝", "白城", "沧州",
+	}
+
+	if len(result.Requests) != resultSize {
+		t.Errorf("result should have %d"+
+			"requests; but had %d",
+			resultSize, len(result.Requests))
+	}
+	for i, url := range expectedUrls {
+		if result.Requests[i].Url != url {
+			t.Errorf("expected url #%d:%s;but"+"was %s",
+				i, url, result.Requests[i].Url)
+		}
+	}
+	if len(result.Items) != resultSize {
+		t.Errorf("result should have %d"+
+			"requests; but had %d",
+			resultSize, len(result.Items))
+	}
+
+	for i, city := range expectedCities {
+		if result.Items[i].(string) != city {
+			t.Errorf("expected city #%d:%s;but"+"was %s",
+				i, city, result.Items[i].(string))
+		}
+	}
+
+}
