0b816c9eae1b106fa3644de6d2e1aab10897c7c7
hqqqqg <2426291059@qq.com>
2026-06-15 15:05:00 +0800

    并发爬虫worker=parse+fetch

--- crawler/engine/engine.go
+++ crawler/engine/engine.go
@@ -14,19 +14,27 @@ func Run(seeds ...Request) { //可以丢任意数量的种子
 	for len(requests) > 0 {
 		r := requests[0]
 		requests = requests[1:]
-		log.Printf("Fetching %s", r.Url)  //打印日志，在抓谁
-		body, err := fetcher.Fetch(r.Url) //抓源码存到body
+		parseResult, err := worker(r)
 		if err != nil {
-			log.Printf("Fetcher: error"+
-				"fetching url %s:%v",
-				r.Url, err)
-			continue //有错就记录，再去处理下一个
+			continue
 		}
-		parseResult := r.ParserFunc(body) //解析器抠出有用数据citylist
 		requests = append(requests,
 			parseResult.Requests...) //...citylist的结果展开一个一个加进去，
 		for _, item := range parseResult.Items {
 			log.Printf("Got item %v", item)
 		}
 	}
+}
+
+// 将parser和fetch合起来
+func worker(r Request) (ParseResult, error) {
+	log.Printf("Fetching %s", r.Url)  //打印日志，在抓谁
+	body, err := fetcher.Fetch(r.Url) //抓源码存到body
+	if err != nil {
+		log.Printf("Fetcher: error"+
+			"fetching url %s:%v",
+			r.Url, err)
+		return ParseResult{}, err //ParseResult是结构体
+	}
+	return r.ParserFunc(body), nil
 }
